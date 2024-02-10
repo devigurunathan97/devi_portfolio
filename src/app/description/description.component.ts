6768f4f7a4e99161feae4b2fb16349380aa98095
devigurunathan97 <devigurunathan97@gmail.com>
2024-02-10 12:29:10 +0530

    10-02-2024 PAYMENT ISSUE

--- src/app/description/description.component.ts
+++ src/app/description/description.component.ts
@@ -6,10 +6,48 @@ import { PortfolioService } from '../portfolio.service';
 @Component({
   selector: 'app-description',
   standalone: true,
-  imports: [HeaderComponent,FooterComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './description.component.html',
   styleUrl: './description.component.scss'
 })
 export class DescriptionComponent {
+  order_id: any
+  createRzpayOrder(data: any) {
+    console.log(data);
+    this.initPay(this.order_id);
+  }
 
+  initPay(val: any) {
+    const options: any = {
+      key: 'rzp_test_key',
+      amount: 125500, // amount should be in paise format to display Rs 1255 without decimal point
+      currency: 'INR',
+      name: '', // company name or product name
+      description: '',  // product description
+      image: './assets/logo.png', // company logo or product image
+      order_id: val, // order_id created by you in backend
+      modal: {
+        // We should prevent closing of the form when esc key is pressed.
+        escape: false,
+      },
+      notes: {
+        // include notes if any
+      },
+      theme: {
+        color: '#0c238a'
+      }
+    };
+    options.handler = ((response: any, error: any) => {
+      options.response = response;
+      console.log(response);
+      console.log(options);
+      // call your backend api to verify payment signature & capture transaction
+    });
+    options.modal.ondismiss = (() => {
+      // handle the case when user closes the form while transaction is in progress
+      console.log('Transaction cancelled.');
+    });
+    const rzp = new this.winRef.nativeWindow.Razorpay(options);
+    rzp.open();
+  }
 }
