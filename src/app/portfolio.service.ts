f04396e943650c531953b7c762efd1d8e9d23d70
devigurunathan97 <devigurunathan97@gmail.com>
2024-02-26 15:19:06 +0530

    URL CHANGED

--- src/app/portfolio.service.ts
+++ src/app/portfolio.service.ts
@@ -6,20 +6,21 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class PortfolioService {
+  url: any = 'http://44.211.166.253:8001';
   constructor(private http: HttpClient) { }
   products() {
-    return this.http.get<any[]>('http://34.229.157.92:3001/products');
+    return this.http.get<any[]>(this.url + '/products');
   }
 
   product(id :any) {
-    return this.http.get<any[]>('http://34.229.157.92:3001/products/' + id);
+    return this.http.get<any[]>(this.url + '/products/' + id);
   }
 
   create_order(amount :any) {
-    return this.http.post<any[]>('http://34.229.157.92:3001/create_order', amount);
+    return this.http.post<any[]>(this.url + '/create_order', amount);
   }
 
   payment_details(payment_data: any): Observable<object> {
-    return this.http.post<any[]>('http://34.229.157.92:3001/fetch_order', payment_data);
+    return this.http.post<any[]>(this.url + '/fetch_order', payment_data);
   }
 }
